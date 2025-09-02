c6b01ed426aa7300201a8682a386e686f3e0c8e5
Nournouas <171356831+Nournouas@users.noreply.github.com>
2025-09-02 17:55:00 +0100

    fix: before making changes

--- webpack.common.js
+++ webpack.common.js
@@ -1,24 +1,26 @@
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
-export const entry = "./src/index.js";
-export const module = {
-  rules: [
-    {
-      test: /\.html$/,
-      use: ["html-loader"],
-    },
-    {
-      test: /\.(png|svg|jpg|jpeg|gif)$/i,
-      type: "asset/resource",
-    },
+export default {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
   ],
-};
-export const plugins = [
-  new HtmlWebpackPlugin({
-    template: "./src/index.html",
-    filename: "index.html",
-  }),
-];
-export const resolve = {
-  extensions: [".js"],
+  resolve: {
+    extensions: [".js"],
+  },
 };
